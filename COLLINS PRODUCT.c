83ef960340a95c4b478fec46a18ab6416632ca54
Collins14734 <101789267+Collins14734@users.noreply.github.com>
2022-03-28 19:47:05 +0300

    Add files via upload

---
+++ COLLINS PRODUCT.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x;
+    int y;
+    int product;
+
+
+    printf("Enter first number");
+    scanf("%d",&x);
+    printf("Enter second number");
+    scanf("%d",&y);
+
+    product=x*y;
+
+    printf("%d\n" , product);
+
+    return 0;
+}
