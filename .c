dd87cf3a2a2a6bb2aba8b4986f914a5d72096358
Collins14734 <101789267+Collins14734@users.noreply.github.com>
2022-04-05 18:54:19 +0300

    Add files via upload

---
+++ .c
@@ -0,0 +1,17 @@
+//program to find sum of five numbers
+#include<stdio.h>
+int main()
+{
+int a=1;
+int s=2;
+int d=3;
+int f=4;
+int g=5;
+int sum;
+sum=a+s+d+f+g;
+printf("the sum of 5 numbers is %d",sum);
+
+
+
+	return 0;
+}
